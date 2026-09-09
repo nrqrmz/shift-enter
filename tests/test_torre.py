143e80c9aa0b855142b0ac107e7c51f637786eb8
Enrique Ramirez <nrqrmz@gmail.com>
2026-09-09 11:49:13 -0600

    feat: modulo torre con las nueve capas

---
+++ tests/test_torre.py
@@ -0,0 +1,18 @@
+import matplotlib
+matplotlib.use("Agg")
+
+from shift_enter import torre
+
+
+def test_la_torre_tiene_nueve_capas():
+    assert len(torre.LAS_CAPAS) == 9
+
+
+def test_la_torre_empieza_en_la_arena_y_acaba_en_el_alumno():
+    assert torre.LAS_CAPAS[0][0] == "Arena y electricidad"
+    assert torre.LAS_CAPAS[-1][0] == "Esta celda"
+
+
+def test_dibujar_pinta_un_rectangulo_por_capa():
+    eje = torre.dibujar()
+    assert len(eje.patches) == 9
